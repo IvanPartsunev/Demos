544ab6e820adbb50a7f10ee73fa3b336c9d90c4d
Ivan Partsunev <ivan.parcunev@gmail.com>
2024-06-20 17:00:57 +0300

    First commit with working set up for Docker

---
+++ celery_demo/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celery_demo.settings")
+
+app = Celery("celery_demo")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
